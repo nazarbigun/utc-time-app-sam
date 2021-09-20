aa4828a1e7cb13b2200ee5d662c7c333f2582476
nazar.bigun <nazar.bigun@gmaiil.com>
2021-09-20 12:46:23 +0100

    ci(app): fix cd
    fix cd

--- auth/src/apps/auth-app.ts
+++ auth/src/apps/auth-app.ts
@@ -1,31 +1,44 @@
+import { SecretsManager, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
 import { AuthApigatewayEvent } from '../common/apigateway/auth-apigateway-event';
 import { AuthorizerPolicyResponse } from '../common/apigateway/authorizer-policy-response';
 
 export class AuthApp {
-    async run(event: AuthApigatewayEvent): Promise<AuthorizerPolicyResponse> {
-        const authHeader = event.authorizationToken;
-        const token = process.env.TOKEN;
+  async run(event: AuthApigatewayEvent): Promise<AuthorizerPolicyResponse> {
+    const authHeader = event.authorizationToken;
 
-        if (authHeader !== token) {
-            return this.generatePolicy('user', 'Deny', event.methodArn);
-        }
+    try {
+      const client = new SecretsManager({
+        region: process.env.AWS_REGION // Your region
+      });
 
-        return this.generatePolicy('user', 'Allow', event.methodArn);
+      const command = new GetSecretValueCommand({ SecretId: process.env.SECRET_NAME });
+      const data = await client.send(command);
+
+      if (authHeader !== data.SecretString) {
+        return this.generatePolicy('user', 'Deny', event.methodArn);
+      }
+
+      return this.generatePolicy('user', 'Allow', event.methodArn);
     }
-
-    private generatePolicy(principalId: string, effect: string, resource: string) : AuthorizerPolicyResponse {
-        return {
-            principalId,
-            policyDocument: {
-                Version: '2012-10-17',
-                Statement: [
-                    {
-                        Action: 'execute-api:Invoke',
-                        Effect: effect,
-                        Resource: resource,
-                    }
-                ]
-            }
-        };
+    catch (err) {
+      console.log(err);
+      throw new Error('You have issue with getting secret.')
     }
+  }
+
+  private generatePolicy(principalId: string, effect: string, resource: string): AuthorizerPolicyResponse {
+    return {
+      principalId,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: effect,
+            Resource: resource
+          }
+        ]
+      }
+    };
+  }
 }
